419199218469a813097e85e581b353b2a940ef6b
Dongjiao <dongjiao-tang@kke.co.jp>
2015-09-24 15:38:27 +0900

    email

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,13 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the Rails application.
 SampleApp::Application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["sg_username"],
+  :password => ENV["sg_password"],
+  :domain => 'herokuapp.com',
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
